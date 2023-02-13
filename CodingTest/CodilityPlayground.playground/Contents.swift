f9eb450c3366d026ae5931b54c6b2ee9d42dd69d
SwiftyCody <soldolly@gmail.com>
2023-02-14 05:56:49 +0900

    프로그래머스 플레이그라운드 및 기능개발 문제 추가
    프로그래머스 플레이그라운드 추가
    기능개발 문제 추가
    폴더 정리

---
+++ CodingTest/CodilityPlayground.playground/Contents.swift
@@ -0,0 +1,82 @@
+import Foundation
+
+var greeting = "Sources의 결과를 확인"
+
+// 1. Iterations
+BinaryGap.solution(1041)
+
+// 2. Arrays
+CyclicRotation.solution([3, 8, 9, 7, 6], 3)
+OddOccurrencesInArray.solution([9, 3, 9, 3, 9, 7, 9])
+
+// 3. Time Complexity
+PermMissingElem.solution([2, 3, 1, 5])
+TapeEquilibrium.solution([3, 1, 2, 4, 3])
+
+// 4. Counting Elements
+FrogRiverOne.solution(5, [1, 3, 1, 4, 2, 3, 5, 4])
+MaxCounters.solution(5, [3, 4, 4, 6, 1, 4, 4])
+MissingInteger.solution([1, 3, 6, 4, 1, 2])
+PermCheck.solution([4, 1, 3, 2])
+
+// 5. Prefix Sums
+CountDiv.solution(6, 11, 2)
+GenomicRangeQuery.solution("CAGCCTA", [2, 5, 0], [4, 5, 6])
+MinAvgTwoSlice.solution([4, 2, 2, 5, 1, 5, 8])
+PassingCars.solution([0 , 1, 0, 1, 1])
+
+// 6. Sorting
+Distinct.solution([2, 1, 1, 2, 3, 1])
+MaxProductOfThree.solution([-3, 1, 2, -2, 5, 6])
+NumberOfDiscIntersections.solution([1, 5, 2, 1, 4, 0])
+Triangle.solution([10, 2, 5, 1, 8, 20])
+
+// 7. StacksAndQueues
+Brackets.solution("{[()()]}")
+Fish.solution([4, 3, 2, 1, 5], [0, 1, 0, 0, 0])
+Nesting.solution("(()(())())")
+StoneWall.solution([8, 8, 5, 7, 9, 8, 7, 4, 8])
+
+// 8. Leader
+Dominator.solution([3, 4, 3, 2, 3, -1, 3, 3])
+EquiLeader.solution([4, 3, 4, 4, 4, 2])
+
+// 9. Maximum slice problem
+MaxProfit.solution([23171, 21011, 21123, 21366, 21013, 21367])
+MaxSliceSum.solution([3, 2, -6, 4, 0])
+
+// 10. Prime and composite numbers
+CountFactors.solution(24)
+Flags.solution([1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2])
+MinPerimeterRectangle.solution(30)
+Peaks.solution([1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2])
+
+// 11. Sieve of Eratosthenes
+CountNonDivisible.solution([3, 1, 2, 3, 6])
+CountSemiprimes.solution(26, [1, 4, 16], [26, 10, 20])
+
+// 12. Euclidean algorithm
+ChocolatesByNumbers.solution(10, 4)
+CommonPrimeDivisors.solution([15, 10, 3], [75, 30, 5])
+
+// 13. Fibonacci numbers
+FibFrog.solution([0, 0, 0, 1, 1, 0, 1, 0, 0 , 0, 0])
+Ladder.solution([4, 4, 5, 5, 1], [3, 2, 4, 3, 1])
+
+// 14. Binary search algorithm
+MinMaxDivision.solution(3, 5, [2, 1, 5, 1, 2, 2, 2])
+NailingPlanks.solution([1, 4, 5, 8], [4, 5, 9, 10], [4, 6, 7, 10, 2])
+
+// 15. Caterpillar method
+AbsDistinct.solution([-5, -3, -1, 0, 3, 6])
+CountDistinctSlices.solution(6, [3, 4, 5, 5, 2])
+CountTriangles.solution([10, 2, 5, 1, 8, 12])
+MinAbsSumOfTwo.solution([-8, 4, 5, -10, 3])
+
+// 16. Greedy algorithms
+MaxNonoverlappingSegments.solution([1, 3, 7, 9, 9], [5, 6, 8, 9, 10])
+TieRopes.solution(4, [1, 2, 3, 4, 1, 1, 3])
+
+// 17. Dynamic Programming
+NumberSolitaire.solution([1, -2, 0, 9, -1, -2])
+MinAbsSum.solution([1, 5, 2, -2])
---
+++ CodingTest/ProgrammersPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+var greeting = "Sources의 결과를 확인"
+
+// 기능개발
+기능개발.solution([93, 30, 55], [1, 30, 5])
+기능개발.solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1])
